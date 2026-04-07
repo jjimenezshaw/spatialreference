4b9b6290f92d928f87aa0299c352ae646d1547fb
Javier Jimenez Shaw <j1@jimenezshaw.com>
2026-04-07 15:38:19 +0200

    proj for factors

--- web/proj/projFunctions.js
+++ web/proj/projFunctions.js
@@ -91,6 +91,28 @@ class Keeper {
     }
 
     /**
+     * Creates a C pointer with the vector of strings as PROJ expects
+     * @param {string[]} options - vector of options to add
+     * @returns {number} - the created pointer
+     */
+    multi_string_options(options) {
+        if (!Array.isArray(options)) {
+            throw Error('multi_string_options expects an array');
+        }
+        const len = options.length;
+        // We need 4*(n + 1) bytes (32-bit pointers: n for the strings, one for NULL)
+        const options_array_ptr = this.malloc((len + 1) * 4);
+        let i = 0;
+        for (; i < len; i++) {
+            const opt = options[i];
+            const opt_ptr = this.string(opt);
+            this.proj.setValue(options_array_ptr + 4 * i, opt_ptr, 'i32');
+        }
+        this.proj.setValue(options_array_ptr + 4 * i, 0, 'i32'); // null terminator.
+        return options_array_ptr;
+    }
+
+    /**
      * Destroys every pointer registered.
      */
     clean() {
@@ -156,30 +178,6 @@ function struct_ptr_to_dict(proj, struct_ptr, params) {
     }
     delete res[dummy];
     return res;
-}
-
-/**
- * Creates a C pointer with the vector of strings as PROJ expects
- * @param {Keeper} keep - memory keeper
- * @param {Proj} proj
- * @param {string[]} options - vector of options to add
- * @returns {number} - the pointer, already in the Keeper
- */
-function multi_string_options(keep, proj, options) {
-    if (!Array.isArray(options)) {
-        throw Error('multi_string_options expects an array');
-    }
-    const len = options.length;
-    // We need 8 bytes (two 32-bit pointers: one for the string, one for NULL)
-    const options_array_ptr = keep.malloc((len + 1) * 4);
-    let i = 0;
-    for (; i < len; i++) {
-        const opt = options[i];
-        const opt_ptr = keep.string(opt);
-        proj.setValue(options_array_ptr + 4 * i, opt_ptr, 'i32');
-    }
-    proj.setValue(options_array_ptr + 4 * i, 0, 'i32'); // null terminator.
-    return options_array_ptr;
 }
 
 /** Class represeting a coordinate Transformer. Do not forget to call dispose() after usage.
@@ -323,7 +321,7 @@ class Transformer {
             const PROJ_5 = 0;
             const PJ_WKT2_2019 = 2;
 
-            const options_array_ptr = multi_string_options(keep, this.proj, ['MULTILINE=YES']);
+            const options_array_ptr = keep.multi_string_options(['MULTILINE=YES']);
             const proj_5 = this.proj._proj_as_proj_string(this.ctx, operation_ptr, PROJ_5, options_array_ptr);
             const wkt2 = this.proj._proj_as_wkt(this.ctx, operation_ptr, PJ_WKT2_2019, 0);
 
@@ -347,6 +345,19 @@ class Transformer {
 class Proj {
     static current_script_url = typeof document !== 'undefined' ? document.currentScript.src : ''; // just for browser
 
+    static PJ_TYPE_UNKNOWN = 0;
+    static PJ_TYPE_DYNAMIC_GEODETIC_REFERENCE_FRAME = 4;
+    static PJ_TYPE_DYNAMIC_VERTICAL_REFERENCE_FRAME = 6;
+
+    static PJ_TYPE_GEOCENTRIC_CRS = 10;
+    static PJ_TYPE_GEOGRAPHIC_2D_CRS = 12;
+    static PJ_TYPE_GEOGRAPHIC_3D_CRS = 13;
+    static PJ_TYPE_VERTICAL_CRS = 14;
+    static PJ_TYPE_PROJECTED_CRS = 15;
+    static PJ_TYPE_COMPOUND_CRS = 16;
+    static PJ_TYPE_OTHER_COORDINATE_OPERATION = 24;
+    static PJ_TYPE_DERIVED_PROJECTED_CRS = 28;
+
     /** Created the Proj object */
     constructor() {
         this.proj;
@@ -354,7 +365,7 @@ class Proj {
         this.init_promise;
         this.ptr_size;
         this.geod_geodesic_ptr; // GRS80. Here for performance.
-        this.user_db = { name: undefined, names: [] };
+        this.user_db = { name: [], aux_names: [] };
     }
 
     /**
@@ -431,49 +442,48 @@ class Proj {
             });
     }
 
+    /**
+     * Set main and/or auxiliary databases.
+     * To reset the main or auxiliary dbs, set a null. undefined will keep the previous state.
+     * Notice that the usage of auxiliary dbs in PROJ slows down the access, even to the main db.
+     * The names of the aux_dbs must be different. They are used in the local wasm FS.
+     * @example
+     * set_database({aux_dbs: [{name:'nsrs_proj.db', array_buffer: ...}] })
+     * @param {Object} args
+     * @param {{name: string, array_buffer: ArrayBuffer}} [args.db] - Name and content of the main db.
+     * @param {{name: string, array_buffer: ArrayBuffer}[]} [args.aux_dbs] - string of name and content of the aux dbs.
+     * @returns {number} - True in case of success, as in proj_context_set_database_path
+     */
     set_database(args) {
         const keep = new Keeper(this.proj);
         try {
-            let name_ptr = 0;
-            let aux_names_ptr = 0;
-            if (args.db === undefined) {
-                name_ptr = this.user_db.name ? keep.string(this.user_db.name) : 0;
-            } else {
-                if (this.user_db.name) {
-                    this.proj.FS.unlink(this.user_db.name);
-                }
-                this.user_db.name = undefined;
-                if (args.db) {
-                    const name = `/${args.db.name}`;
-                    const array_buffer = args.db.array_buffer;
-                    const uint8_array = new Uint8Array(array_buffer);
-                    this.proj.FS.writeFile(name, uint8_array);
-                    this.user_db.name = name;
-                    name_ptr = keep.string(name);
-                }
-            }
-
-            if (args.aux_dbs === undefined) {
-                aux_names_ptr = this.user_db.names ? multi_string_options(keep, this.proj, this.user_db.names) : 0;
-            } else {
-                for (const to_unlink of this.user_db.names) {
+            const process_dbs = (names, dbs) => {
+                for (const to_unlink of names) {
                     this.proj.FS.unlink(to_unlink);
                 }
-                this.user_db.names = [];
-                if (args.aux_dbs) {
-                    const aux_names = [];
-                    args.aux_dbs.forEach((aux_db) => {
-                        const name = `/${aux_db.name}`;
-                        const array_buffer = aux_db.array_buffer;
-                        const uint8_array = new Uint8Array(array_buffer);
-                        this.proj.FS.writeFile(name, uint8_array);
-                        this.user_db.names.push(name);
-                        aux_names.push(name);
+                names.splice(0); // delete the content, keep the same object by ref.
+                if (dbs) {
+                    dbs.forEach((db) => {
+                        if (db) {
+                            const name = `/${db.name}`;
+                            const uint8_array = new Uint8Array(db.array_buffer);
+                            this.proj.FS.writeFile(name, uint8_array);
+                            names.push(name);
+                        }
                     });
-
-                    aux_names_ptr = multi_string_options(keep, this.proj, aux_names);
                 }
+            };
+
+            if (args.db !== undefined) {
+                process_dbs(this.user_db.name, [args.db]);
             }
+            const name_ptr = this.user_db.name.length ? keep.string(this.user_db.name[0]) : 0;
+
+            if (args.aux_dbs !== undefined) {
+                process_dbs(this.user_db.aux_names, args.aux_dbs);
+            }
+            const aux_names_ptr = this.user_db.aux_names ? keep.multi_string_options(this.user_db.aux_names) : 0;
+
             const res = this.proj._proj_context_set_database_path(this.ctx, name_ptr, aux_names_ptr, 0);
             return res;
         } finally {
@@ -592,7 +602,6 @@ class Proj {
         }
         const ptr_size = this.ptr_size;
         const double_size = 8; // Doubles are 8 bytes
-        const PJ_TYPE_COMPOUND_CRS = 16; // from proj.h
         const keep = new Keeper(this.proj);
         let res = {};
         try {
@@ -635,7 +644,7 @@ class Proj {
             };
             const sourceCRS = keep.string(args.crs);
             const P_crs = keep.call('_proj_create', this.ctx, sourceCRS);
-            if (this.proj._proj_get_type(P_crs) === PJ_TYPE_COMPOUND_CRS) {
+            if (this.proj._proj_get_type(P_crs) === Proj.PJ_TYPE_COMPOUND_CRS) {
                 const P_crs_0 = keep.call('_proj_crs_get_sub_crs', this.ctx, P_crs, 0);
                 const P_crs_1 = keep.call('_proj_crs_get_sub_crs', this.ctx, P_crs, 1);
                 res = internal_axes(P_crs_0);
@@ -708,7 +717,9 @@ class Proj {
 
             const ctx = this.proj._proj_context_clone(this.ctx);
             this.proj._proj_context_set_enable_network(ctx, args.use_network);
-            let P = this.proj._proj_create_crs_to_crs_from_pj(ctx, P_src, P_tgt, area, 0);
+            // In case of use_network, force "only best", to not provide a wrong result if there is a network problem
+            const options_array_ptr = args.use_network ? keep.multi_string_options(['ONLY_BEST=YES']) : 0;
+            let P = this.proj._proj_create_crs_to_crs_from_pj(ctx, P_src, P_tgt, area, options_array_ptr);
             if (P === 0) {
                 this.proj._proj_destroy(ctx);
                 throw new Error('proj_create_crs_to_crs_from_pj returned NULL.');
@@ -821,19 +832,16 @@ class Proj {
         }
         const keep = new Keeper(this.proj);
         const res = {};
-        const PJ_TYPE_UNKNOWN = 0;
-        const PJ_TYPE_DYNAMIC_GEODETIC_REFERENCE_FRAME = 4;
-        const PJ_TYPE_DYNAMIC_VERTICAL_REFERENCE_FRAME = 6;
         try {
             const crs = keep.string(args?.crs);
             const P_crs = keep.call('_proj_create', this.ctx, crs);
             const is_crs = !!this.proj._proj_is_crs(P_crs);
             const P_datum = is_crs ? keep.call('_proj_crs_get_datum_forced', this.ctx, P_crs) : 0;
-            res.type = P_datum === 0 ? PJ_TYPE_UNKNOWN : this.proj._proj_get_type(P_datum);
+            res.type = P_datum === 0 ? Proj.PJ_TYPE_UNKNOWN : this.proj._proj_get_type(P_datum);
             res.name = P_datum === 0 ? '' : this.proj.UTF8ToString(this.proj._proj_get_name(P_datum));
             res.is_dynamic =
-                res.type === PJ_TYPE_DYNAMIC_GEODETIC_REFERENCE_FRAME ||
-                res.type === PJ_TYPE_DYNAMIC_VERTICAL_REFERENCE_FRAME;
+                res.type === Proj.PJ_TYPE_DYNAMIC_GEODETIC_REFERENCE_FRAME ||
+                res.type === Proj.PJ_TYPE_DYNAMIC_VERTICAL_REFERENCE_FRAME;
             return res;
         } finally {
             keep.clean();
@@ -894,6 +902,57 @@ class Proj {
     }
 
     /**
+     * Gets the geographic CRS underlying a projected, compound, etc
+     * @param {Object} args
+     * @param {string} args.crs - definition of the CRS
+     * some old PROJ4 expressions need a "+type=crs" to make it clear its nature.
+     * @returns {string} - geographic CRS.
+     */
+    crs_get_geographic(args) {
+        if (!args?.crs?.length) {
+            throw Error(`args.crs is mandatory.`);
+        }
+        if (typeof args.crs !== 'string') {
+            throw Error(`args.crs must be a string.`);
+        }
+        const keep = new Keeper(this.proj);
+        try {
+            const crs = keep.string(args.crs);
+            let P_ref = keep.call('_proj_create', this.ctx, crs);
+            if (P_ref === 0) {
+                throw Error(`cannot create valid object from this: ${args.crs}`);
+            }
+
+            let type = this.proj._proj_get_type(P_ref);
+            if (type === Proj.PJ_TYPE_GEOCENTRIC_CRS) {
+                throw Error(`cannot get geographic from geocentric CRS`);
+            } else if (type === Proj.PJ_TYPE_VERTICAL_CRS) {
+                throw Error(`cannot get geographic from vertical CRS`);
+            } else if (type === Proj.PJ_TYPE_COMPOUND_CRS) {
+                P_ref = keep.call('_proj_crs_get_sub_crs', this.ctx, P_ref, 0);
+                type = this.proj._proj_get_type(P_ref);
+            }
+
+            if (type === Proj.PJ_TYPE_GEOGRAPHIC_2D_CRS || type === Proj.PJ_TYPE_GEOGRAPHIC_3D_CRS) {
+                // nothing to do
+            } else if (type === Proj.PJ_TYPE_PROJECTED_CRS) {
+                P_ref = keep.call('_proj_get_source_crs', this.ctx, P_ref);
+            } else if (type === Proj.PJ_TYPE_DERIVED_PROJECTED_CRS) {
+                P_ref = keep.call('_proj_get_source_crs', this.ctx, P_ref);
+                P_ref = keep.call('_proj_get_source_crs', this.ctx, P_ref);
+            } else {
+                throw Error(`Unsupported type ${type} to extract the geographic system`);
+            }
+            const PJ_WKT2_2019 = 2;
+            const wkt2 = this.proj._proj_as_wkt(this.ctx, P_ref, PJ_WKT2_2019, 0);
+            const out = this.proj.UTF8ToString(wkt2);
+            return out;
+        } finally {
+            keep.clean();
+        }
+    }
+
+    /**
      * @typedef {Object} factors_result
      * @property {number} meridional_scale -
      * @property {number} parallel_scale
@@ -907,8 +966,9 @@ class Proj {
      * @property {number} dx_dphi
      * @property {number} dy_dlam
      * @property {number} dy_dphi
-     * @property {number} lat - degrees
-     * @property {number} lon - degrees
+     * @property {number} latitude - degrees
+     * @property {number} longitude - degrees
+     * @property {number} ellipsoidal_height_m - metres
      * @property {number} error_code - error generated by PROJ. 0 on success.
      * @property {string} [error_msg] - error message when error_code is not 0.
      */
@@ -937,6 +997,26 @@ class Proj {
                 throw Error(`cannot create valid object from this: ${args.crs}`);
             }
 
+            const P_ellipsoid = keep.call('_proj_get_ellipsoid', this.ctx, P_crs);
+            let semi_mayor_m = 0;
+            let e2 = 0;
+            if (P_ellipsoid) {
+                const double_size = 8;
+                const out_semi_mayor_m_ptr = keep.malloc(double_size);
+                const out_semi_minor_m_ptr = keep.malloc(double_size);
+                this.proj._proj_ellipsoid_get_parameters(
+                    this.ctx,
+                    P_ellipsoid,
+                    out_semi_mayor_m_ptr,
+                    out_semi_minor_m_ptr,
+                    0,
+                    0,
+                );
+                semi_mayor_m = this.proj.getValue(out_semi_mayor_m_ptr, 'double');
+                const semi_minor_m = this.proj.getValue(out_semi_minor_m_ptr, 'double');
+                e2 = 1 - semi_minor_m ** 2 / semi_mayor_m ** 2;
+            }
+
             const coord_ptr = keep.malloc(32);
             this.proj.setValue(coord_ptr + 8 * 2, 0, 'double'); // z
             this.proj.setValue(coord_ptr + 8 * 3, 0, 'double'); // t
@@ -963,12 +1043,14 @@ class Proj {
                 }
                 const lat = point.lat ?? point[0];
                 const lon = point.lon ?? point[1];
+                const alt = point.alt ?? (point.length >= 3 ? point[2] : 0);
                 const lat_rad = this.proj._proj_torad(lat);
                 const lon_rad = this.proj._proj_torad(lon);
 
                 this.proj.setValue(coord_ptr + 8 * 0, lon_rad, 'double');
                 this.proj.setValue(coord_ptr + 8 * 1, lat_rad, 'double');
 
+                this.proj._proj_errno_reset(P_crs);
                 this.proj._proj_factors(factors_ptr, P_crs, coord_ptr);
                 const error_code = this.proj._proj_errno(P_crs);
                 if (error_code) this.proj._proj_errno_reset(P_crs);
@@ -977,8 +1059,16 @@ class Proj {
                 for (const key of ['angular_distortion', 'meridian_parallel_angle', 'meridian_convergence']) {
                     elem[key] = this.proj._proj_todeg(elem[key]);
                 }
-                elem.lat = lat;
-                elem.lon = lon;
+                const gaussian_r = semi_mayor_m
+                    ? (semi_mayor_m * Math.sqrt(1 - e2)) / (1 - e2 * Math.sin(lat_rad) ** 2)
+                    : NaN;
+                elem.elevation_factor = gaussian_r / (gaussian_r + alt);
+                if (Math.abs(elem.meridional_scale - elem.parallel_scale) < 1e-5) {
+                    elem.combined_factor = elem.meridional_scale * elem.elevation_factor;
+                }
+                elem.latitude = lat;
+                elem.longitude = lon;
+                elem.ellipsoidal_height_m = alt;
                 elem.error_code = error_code;
                 if (error_code) {
                     const msgPtr = this.proj._proj_context_errno_string(this.ctx, error_code);
